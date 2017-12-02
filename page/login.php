fb95f83aad4656a8fecd286579d5f8ee23665800
Vlado Repic <vlado.repic@gmail.com>
2017-12-02 10:20:27 +0100

    Create login.php

---
+++ page/login.php
@@ -0,0 +1,22 @@
+<div class="uk-box-shadow-medium uk-padding uk-position-center uk-text-center">
+	<img class="uk-margin-bottom" style="vertical-align: middle;" width="140" height="120" src="assets/img/logo.png" alt="CYP Logo">
+	<form>
+	    <div class="uk-margin">
+	        <div class="uk-inline">
+	            <span class="uk-form-icon" uk-icon="icon: user"></span>
+	            <input class="uk-input" type="text" placeholder="E-Mail">
+	        </div>
+	    </div>
+	    <div class="uk-margin">
+	        <div class="uk-inline">
+	            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+	            <input class="uk-input" type="text" placeholder="Passwort">
+	    </div>
+	    <div class="uk-margin" uk-margin>
+	        <button class="uk-button uk-button-primary">Login</button>
+	    </div>
+	    <div class="uk-text-center-small">
+            <a class="uk-link-muted" href="#">Passwort vergessen?</a>
+    	</div>
+	</form>
+</div>
