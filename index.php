45d4199c4a9914dff95d80d8f230d50b3bc7f3d6
Vlado Repic <vlado.repic@gmail.com>
2017-12-02 10:19:32 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+	ini_set('error_reporting',E_ALL ^ E_NOTICE);
+
+?>
+
+<!DOCTYPE html>
+<head>
+<?php
+	$page=@$_GET['page'];
+	$pages=array('home','medien');
+	//require_once(('meta/'.(in_array($page,$pages) ? $page : 'home')).'.php');
+?>
+	<title>Medienübersicht &ndash; CYP Mediendatenbank</title>
+    <link rel="stylesheet" href="assets/css/uikit.min.css" />
+</head>
+<body>
+	<div class="uk-container uk-margin">
+<?php
+	require_once('include/header.php'); 
+	$page=@$_GET['page'];
+	$pages=array('home','upload','login');
+	require_once(('page/'.(in_array($page,$pages) ? $page : 'medienuebersicht')).'.php');
+	require_once('include/footer.php');	
+?>
+	</div>
+	<script
+	  src="https://code.jquery.com/jquery-3.2.1.min.js"
+	  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+	  crossorigin="anonymous"></script>
+	<script src="assets/js/uikit.min.js"></script>
+	<script src="assets/js/uikit-icons.min.js"></script>
+</body>
+</html>
