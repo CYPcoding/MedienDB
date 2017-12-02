2b77c6509b7b4ac5a3a4d71bf6304dd254fda27f
Vlado Repic <vlado.repic@gmail.com>
2017-12-02 10:20:43 +0100

    Create medienuebersicht.php

---
+++ page/medienuebersicht.php
@@ -0,0 +1,223 @@
+<div uk-grid>
+	<div class="uk-width-1-2@m">
+		<ul class="uk-subnav uk-subnav-pill"  uk-margin>
+	    	<li><a href="#">Alle</a></li>
+	    	<li class="uk-active"><a href="#">Bilder</a></li>
+	    	<li><a href="#">Videos</a></li>
+		</ul>
+	</div>
+
+	<div class="uk-width-1-2@m" align="right">
+		<a class="uk-badge">Logo</a>
+		<a class="uk-badge">Corporate</a>
+		<a class="uk-badge">BFE</a>
+		<a class="uk-badge">Icon</a>
+		<a class="uk-badge">PAB</a>
+		<a class="uk-badge">BFK</a>
+		<a class="uk-badge">BEM</a>
+	</div>
+</div>
+
+<div class="uk-child-width-1-5@m" uk-grid>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+                <span class="uk-badge">Corporate</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-media-top">
+                <img src="assets/img/kermit.jpg" alt="">
+            </div>
+            <div class="uk-card-body">
+                <span class="uk-badge">BFE</span>
+            </div>
+        </div>
+    </div>
+</div>
