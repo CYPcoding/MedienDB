6d82b0b1ccea31fcc156ebae11771e0ad800a8d6
Vlado Repic <vlado.repic@gmail.com>
2017-12-02 10:21:01 +0100

    Create upload.php

---
+++ page/upload.php
@@ -0,0 +1,10 @@
+<div class="uk-child-width-1-2@m uk-position-center uk-margin" uk-grid>
+	<div class="uk-child-width-1-2@m uk-card uk-card-default uk-text-center uk-card-body uk-float-left">
+		<span class="uk-float-left" uk-icon="icon: image; ratio: 2"></span>
+		<p class="uk-float-right">Neues Bild erfassen</p>
+	</div>
+	<div class="uk-child-width-1-2@m uk-card uk-card-default uk-text-center uk-card-body uk-float-right">
+		<span class="uk-float-left" uk-icon="icon: video-camera; ratio: 2"></span>
+		<p class="uk-float-right">Neues Video erfassen</p>
+	</div>
+</div>
