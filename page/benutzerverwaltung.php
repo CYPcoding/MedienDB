dab34a2935c363ea12f588986076adef6331b22c
Vlado Repic <vlado.repic@gmail.com>
2017-12-20 13:37:11 +0100

    adds user management functionality

--- page/benutzerverwaltung.php
+++ page/benutzerverwaltung.php
@@ -34,7 +34,9 @@ if($sql_alluser_resultCheck > 0){
 			<th>ID</th>
 			<th>Name</th>
 			<th>E-Mail</th>
+			<th></th>
 			<th>Rolle</th>
+			<th></th>
 		</tr>
 	</thead>
 	<tbody>
@@ -44,12 +46,46 @@ if($sql_alluser_resultCheck > 0){
 				<td>' . $row_u['id'] . '</td>' . '
 				<td>' . $row_u['name'] . '</td>' . '
 				<td>' . $row_u['email'] . '</td>' . '
+				<td></td>' . '
 				<td>' . $row_u['role'] . '</td>' . '
-			  </tr>
+				<td></td>' . '
+			  </tr>';
+	} echo '
+			  <form action="benutzerverwaltung" method="POST">
+				  <tr>
+					  <td></td>
+					  <td>
+					  	<input class="uk-input" type="text" name="newuserName" placeholder="Peter Muster" value="';
+					  	if(isset($_POST['newuserName'])){ echo $_POST['newuserName']; }
+					  	echo '">
+					  </td>
+					  <td>
+					  	<input class="uk-input" type="text" name="newuserEmail" placeholder="peter.muster@example.ch" value="';
+					  	if(isset($_POST['newuserEmail'])){ echo $_POST['newuserEmail']; }
+					  	echo '">
+					  </td>
+					  <td>
+					  	<input class="uk-input" type="password" name="newuserPassword" placeholder="Passwort" value="';
+					  	if(isset($_POST['newuserPassword'])){ echo $_POST['newuserPassword']; }
+					  	echo '">
+					  </td>
+
+					  <td>
+					  	<select class="uk-select" name="newuserRole">
+					  		<option value="Admin">Admin</option>
+					  		<option value="User">User</option>
+					  	</select>
+					  </td>
+					  <td>
+					  	<button class="uk-button uk-button-primary" type="submit" name="newuserEntry">neuen Benutzer speichern</button>
+					  </td>
+				  </tr>
+			  </form>
+			  <?php
 			  ';
-	}
 	echo '</tbody>
-		</table>';
+		</table>
+		<p><span class="uk-text-danger">' . $newusernameError . '<br>' . $newuseremailError . '<br>' . $newuserroleError . '</p>';
 } else {
 	echo 'Keine Eintr&auml;ge';
 }
--- page/meta/benutzerverwaltung.php
+++ page/meta/benutzerverwaltung.php
@@ -8,4 +8,32 @@ if( !isset($_SESSION['email']) || $userRole == 'User' ) {
 $_SESSION['page'] = 'benutzerverwaltung';
 $_SESSION['s'] = $_GET['s'];
 
+$error = false;
+
+if(isset($_POST['newuserEntry'])) {
+
+	$newuserName = $_POST['newuserName'];
+	$newuserEmail = $_POST['newuserEmail'];
+	$newuserPassword = $_POST['newuserPassword'];
+	$newuserRole = $_POST['newuserRole'];
+
+	if(empty($newuserName)){
+		$error = true;
+		$newusernameError = "Bitte geben Sie den Vor- und Nachnamen an.";
+	}
+	if(empty($newuserEmail)){
+		$error = true;
+		$newuseremailError = "Bitte geben Sie die E-Mailadresse an.";
+	}
+	if(empty($newuserPassword)){
+		$error = true;
+		$newuserpasswordError = "Bitte geben Sie ein Passwort an.";
+	}
+	if(!$error) {
+		$sql_newuser_entry = "INSERT INTO users (name, email, password, role) VALUES ('$newuserName', '$newuserEmail', '$newuserPassword', '$newuserRole');";
+		mysqli_query($conn, $sql_newuser_entry);
+
+	}
+}
+
 ?>
